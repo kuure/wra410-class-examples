15bd203874276f6b7ce4ab0ed71cd2a688d46b8c
kuure <kuure@msu.edu>
2024-11-03 22:43:52 -0500

    added weatherApp folder and zip file

---
+++ week11/README.md
@@ -0,0 +1,14 @@
+# Week 11
+
+## 11/04/2024 - 11/06/2024
+
+
+### Monday
+
+Let's make a weather app
+
+
+
+
+### Wednesday
+
---
+++ week11/weatherApp/weatherApp.js
@@ -0,0 +1,31 @@
+// where we are putting the output
+const container = document.querySelector(".container")	
+
+// where we get the data from 
+const url = "https://api.weather.gov/gridpoints/GRR/82,39"
+
+// get the data, if it works then pass it to the function named
+// "handleRemoteData(), otherwise print an error
+fetchRemoteData()
+	.then(results => handleRemoteData(results) ) 
+	.catch(error => console.log(error))
+
+
+// this is where our code goes
+function handleRemoteData(jsonData) {
+	console.log(jsonData)
+}
+
+
+
+// don't even worry about this one
+// you can pretend it doesn't exist
+async function fetchRemoteData() {
+  const response = await fetch(url)
+	if(!response.ok) {
+    throw new Error(`HTTP error status: ${response.status}`)
+	}
+  return await response.json()
+}
+
+
