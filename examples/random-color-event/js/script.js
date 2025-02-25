3b7331e5563415f925ba813b9c5b5bce32de6d9a
kuure <kuure@msu.edu>
2025-02-25 18:49:15 -0500

    small change to event handler

--- examples/random-color-event/js/script.js
+++ examples/random-color-event/js/script.js
@@ -3,13 +3,7 @@ const myButton = document.querySelector("button");
 
 // add an event listener for the 'click' event
 myButton.addEventListener("click", () => {
-	const newColor = randomRGBColor()
-	console.log(newColor)
-})
-
-
-// add an event listener for the 'mouseout' event
-myButton.addEventListener("mouseout", () => {
+	console.log("you clicked the button")
 })
 
 
@@ -19,8 +13,15 @@ myButton.addEventListener("mouseover", () => {
 })
 
 
+// add an event listener for the 'mouseout' event
+myButton.addEventListener("mouseout", () => {
+	console.log("your mouse exited the button");
+})
 
-// what the heck does this do?
+
+
+// what the heck does this do,
+// and what can you use it for?
 function randomRGBColor() {
 	let r = Math.floor(Math.random() * 255);
 	let g = Math.floor(Math.random() * 255);
