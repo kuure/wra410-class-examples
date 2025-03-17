4941e5076d74980bed9566a0a72f3cd22edf2a58
kuure <kuure@msu.edu>
2025-03-17 10:52:06 -0400

    changed js api example

--- examples/javascript-api/script.js
+++ examples/javascript-api/script.js
@@ -1,33 +1,44 @@
-// the URL that returns your JSON
-// this example uses placeholder data
+// the url for any API that returns JSON data
 const url = "https://jsonplaceholder.typicode.com/users"
 
 
-// call the function to set everything in motion
-fetchRemoteData(url)
+// this function deals with your data, you can customize
+// it to do whatever you need to do
+function buildPage(jsonData) {
 
-
-// this function deals with your data
-function buildPage(apiData) {
-
-	// log the result
-	console.log(apiData)
+	// for now, just log the result
+	console.log(jsonData)
 
 	// what else do you want to do with it...?
 
 }
 
 
+// call the function at the bottom to set everything in motion
+// it will fetch the data from the URL, then call the buildPage()
+// function above with the results in JSON format
+fetchRemoteData(url)
 
 
 
-// this function handles your business for you
-// and passes the results to the function above
-// you don't need to chane anything here
+/* 
+
+fetchRemoteData() - 
+
+this function is passed a URL as an argument, and uses the JavaScript
+fetch() tool to get the remote data in the background
+
+if the respons from the server is not ok, it prints an error message
+
+otherwise, it takes the results, turns them into JSON, and passes them to
+the function named "buildPage()"
+
+*/
+
 async function fetchRemoteData(location) {
 	const response = await fetch(location)
 	if (!response.ok) {
-    throw new Error(`An error has occured: ${response.status}`)
+		throw new Error(`An error has occured: ${response.status}`)
 	}
 	const jsonResponse = await response.json()
 	buildPage(jsonResponse)	
