b1d5e6b484725a399966e000420a4244d4179c77
kuure <kuure@msu.edu>
2025-03-31 10:08:55 -0400

    fixed nav template, added spongebob

---
+++ examples/spongebob-case/js/script.js
@@ -0,0 +1,23 @@
+// variables to handle the two things we need to interact with 
+const input = document.querySelector(".inputBox") 
+const output = document.querySelector(".outputBox")
+
+
+// 7. code that listens for the input event on the text box
+input.addEventListener('input', () => {
+
+	// 8. code that grabs the current value of the input box
+	
+
+	// 9. code that randomly upper or lowercases each letter of the string
+
+
+	// 10. code that puts the text from the input into the output container
+
+
+})
+
+
+
+
+
