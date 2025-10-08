85546a1bb9cbf6fbcd061c4cfeea124adb47006b
kuure <kuure@msu.edu>
2025-10-08 16:20:24 -0400

    Complete, working version added

--- demos/meme-generator/js/script.js
+++ demos/meme-generator/js/script.js
@@ -1,1 +1,30 @@
+// what is important?
 
+// - input with id 'noInput'
+// - input with id 'yesInput'
+// - p with class 'noText'
+// - p with class 'yesText'
+
+const noBox = document.querySelector("#noInput");
+const noText = document.querySelector(".noText");
+
+const yesBox = document.querySelector("#yesInput");
+const yesText = document.querySelector(".yesText");
+
+
+// how will it work?
+
+// get the current value from yes input whenever 
+// anything is typed (the 'input' event), then 
+// make that value the p tag's innerHTML
+yesBox.addEventListener("input",() => {
+	console.log(yesBox.value);
+	yesText.innerHTML  = yesBox.value;
+})
+
+
+// - same but with the no values
+noBox.addEventListener("input",() => {
+	console.log(noBox.value);
+	noText.innerHTML  = noBox.value;
+})
