5e12d8ac3676026feb60944855d6ef2c0526547f
kuure <kuure@msu.edu>
2025-10-29 15:48:36 -0400

    working version

--- demos/javascript-api/script.js
+++ demos/javascript-api/script.js
@@ -1,59 +1,42 @@
-// the url for any API that returns JSON data
-const url = "https://jsonplaceholder.typicode.com/users"
+// the url where we get the JSON data
+const url = 'https://jsonplaceholder.typicode.com/users'; 
 
-const container = document.querySelector(".container")
-
-// call the function at the bottom to set everything in motion
-// it will fetch the data from the URL, then call the buildPage()
-// function above with the results in JSON format
-fetchRemoteData(url)
+// the html container
+const container = document.querySelector('.container');
 
 
 
-// this function deals with your data, you can customize
-// it to do whatever you need to do
-function buildPage(jsonData) {
 
-	// for now, just log the result
-	console.log(jsonData)
+// run everything by calling the 'doTheWholeThing()' function with the URL as an argument
+doTheWholeThing(url);
 
-	// what else do you want to do with it...?
 
-	const newHTML = jsonData.map( person => {
+// the function that runs everything
+async function doTheWholeThing(url) {
+	// get the data
+	const data = await getData(url);
+	// pass the data and contiainer to the function that formats everything
+	formatData(data,container);
+}
+
+
+
+// the function that gets the data
+async function getData(url) {
+	const response = await fetch(url);
+	const data = await response.json();
+	return data;
+}
+
+
+
+// the unction that processes the data and formatted HTML
+function formatData(data) {
+	const newHTML = data.map( person => {
 		return(`
 			<h1>${person.name}</h1>
 			<h2>${person.phone}</h2>
 		`)
 	}).join("")
-
-	container.innerHTML = newHTML
-
+	return(newHTML);
 }
-
-
-
-
-
-/* 
-
-fetchRemoteData() - 
-
-this function is passed a URL as an argument, and uses the JavaScript
-fetch() tool to get the remote data in the background
-
-if the respons from the server is not ok, it prints an error message
-
-otherwise, it takes the results, turns them into JSON, and passes them to
-the function named "buildPage()"
-
-*/
-
-async function fetchRemoteData(location) {
-	const response = await fetch(location)
-	if (!response.ok) {
-		throw new Error(`An error has occured: ${response.status}`)
-	}
-	const jsonResponse = await response.json()
-	buildPage(jsonResponse)	
-}
-
