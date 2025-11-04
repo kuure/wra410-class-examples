7576824c8a27b7c2058ad800a6bad60df589bb12
kuure <kuure@msu.edu>
2025-11-04 09:23:16 -0500

    finalized version of weather app

--- demos/weather-api/js/weatherScript.js
+++ demos/weather-api/js/weatherScript.js
@@ -1,48 +1,88 @@
-// the weather API 
-const url = "https://api.weather.gov/gridpoints/GRR/82,39/"
-const forecast = "https://api.weather.gov/gridpoints/GRR/82,39/forecast"
-const hourly = "https://api.weather.gov/gridpoints/GRR/82,39/forecast/hourly"
+// the hourly forecast API 
+const url = "https://api.weather.gov/gridpoints/GRR/82,39/forecast/hourly"
 
 // the html container
 const container = document.querySelector(".container");
 
 
 // immediately call the function that runs all the other functions
-doTheWholeThing();
+weatherApp();
 
-async function doTheWholeThing() {
 
-	// first, get the remote data
+
+
+
+// the main weatherApp() function
+async function weatherApp() {
+	// get the data
 	const data = await getData();
-	// when we have it, print to the console
-	console.log(data);
-
-	// then pass the data to a function that does *something* to it
+	// process the data
 	const result = processData(data);
-	
-	// finally, print the processed data to the console 
-	// and insert the results into the container element
-	console.log(result);
+	// put it into the HTML
 	container.innerHTML = result;
 }
 
 
 
-// the function that fetches the remote data
+
+// this gets the data, you can ignore this
 async function getData() {
-	const response = await fetch(hourly);
-	// check for any errors
+	const response = await fetch(url);
 	if (!response.ok) {
 		throw new Error(`An error has occured: ${response.status}`)
 	}
-	// if no errors, return the results
 	const json = await response.json();
 	return json; 
 }
 
 
 
-// the function that processes the data into HTML
+
+// this processes the data, you need to customize this
 function processData(dataArray) {
-	return(dataArray);
+
+	// today is always the first element in the 'periods' array
+	// we can assign this to a variable to simplify referring to it
+	const today = dataArray.properties.periods[0];
+
+	// just get the 'temperature' property from the 'today' variable
+	const temp = today.temperature;
+
+	// create a variable that does not have a value assigned yet
+	// we need to give this a value later so we use 'let'
+	let style;
+
+
+	// if the temp is above 80 
+	if(temp >= 80) {
+		style ="hot";
+	}
+	
+	// if the temp is less than 80 AND equal or greater than 65
+	else if(temp < 80 && temp >= 65) {
+		style ="warm";
+	}
+
+	// if the temp is less than 65 AND equal or greater than 40
+	else if(temp < 65 && temp >= 40) {
+		style ="chilly";
+	}
+	
+  // if the temp is less than 40
+	else if(temp < 40) {
+		style = "cold"
+	}
+
+	// now 'style' should be one of four words that you can use in CSS
+	// this applies to styles to each div - the 'weather' one that all
+	// share, plus the 'style' one that customizes the background color
+	const output = `
+	<div class="weather ${style}">
+		${temp}
+	</div>
+	`;
+
+	// return the formatted string of text
+	return(output)
+
 }
