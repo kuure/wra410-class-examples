1f2df84693c9ad87d1e37e45ff56abea9ad082b4
kuure <kuure@msu.edu>
2025-11-17 14:51:10 -0500

    added spongebob case example

---
+++ demos/spongebob-case/js/script.js
@@ -0,0 +1,14 @@
+// variables to handle the two things we need to interact with
+const input = document.querySelector('.inputBox');
+const output = document.querySelector('.outputBox');
+
+// code that listens for the input event on the text box
+input.addEventListener('input', () => {
+	
+	// code that grabs the current value of the input box
+	
+	// code that randomly upper or lowercases each letter of the string
+	
+	// code that puts the text from the input into the output container
+
+});
