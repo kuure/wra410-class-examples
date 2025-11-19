fdd82eb4a5b198440fe7b55ee2da28686b70dddc
kuure <kuure@msu.edu>
2025-11-19 14:31:27 -0500

    finished version with clear button

--- demos/spongebob-case/js/script.js
+++ demos/spongebob-case/js/script.js
@@ -1,19 +1,28 @@
-// variables to handle the two things we need to interact with
+// variables to handle the three things we need to interact with
 const input = document.querySelector('.inputBox');
 const output = document.querySelector('.outputBox');
+const button = document.querySelector('.clear');
 
 // create an empty string with 'let' so its value can change
 let outputString = "";
 
 
+// reset the text box and image text with a button
+button.addEventListener('click',() => {
+	input.value = "";
+	output.innerHTML= "";
+});
+
+
 // code that listens for the input event on the text box
-input.addEventListener('input', (event) => {
+input.addEventListener('input', (e) => {
 	
-	// the "event" tells us all sorts of info about what the user did
-	console.log(event);
+	// putting a variable name in the parentheses gives you access to
+	// all the information about the event
+	console.log(e);
 
 	// the 'data' property is the most recent change
-	const letter = event.data;
+	const letter = e.data;
 
 	// check if a number between 0 and 0.999 is greater than or 
 	// less than 0.5, so essentially a coin toss
@@ -31,4 +40,5 @@ input.addEventListener('input', (event) => {
 	output.innerHTML = outputString;
 
 });
+
 
