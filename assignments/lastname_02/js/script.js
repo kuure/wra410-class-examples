d9cbba5b56f3f250d2d6e2abe34fa4d84ea551a0
kuure <kuure@msu.edu>
2026-02-22 16:45:19 -0500

    added final assignment 2 files

--- assignments/lastname_02/js/script.js
+++ assignments/lastname_02/js/script.js
@@ -1,17 +1,13 @@
-// selectors 
-const mouseoverMessage= document.querySelector(".mouseoverMessage");
+// selector for the element 
 const clickButton = document.querySelector(".clickButton");
 
-// basic mouseover listener
-mouseoverMessage.addEventListener("mouseover", () => {
-	console.log("element was mousedover-ed");
-})
 
-
-// log the "event" when the button is clicked
+// listen for the 'click' event, then do two things:
+// - print literal text to the console
+// - print all the info about the event that triggered this
 clickButton.addEventListener("click", (event) => {
+	console.log("Click Event On Button");
 	console.log(event);
 })
-
 
 
