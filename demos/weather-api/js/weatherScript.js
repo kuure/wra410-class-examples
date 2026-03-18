b2abe093d69f1c00839afb6018c6de4715d48dd0
kuure <kuure@msu.edu>
2026-03-18 09:37:29 -0400

    added blank version of weather API

--- demos/weather-api/js/weatherScript.js
+++ demos/weather-api/js/weatherScript.js
@@ -24,46 +24,14 @@ async function weatherApp() {
 
 
 
-// this processes the data, you need to customize this
 function processData(dataArray) {
 
-	// today is always the first element in the 'periods' array
-	// we can assign this to a variable to simplify referring to it
-	const today = dataArray.properties.periods[0];
-
-	// just get the 'temperature' property from the 'today' variable
-	const temp = today.temperature;
-
-	// create a var that does not have a value assigned yet
-	// we need to give this a value later so we use 'let'
-	let style;
-
-
-	// if the temp is above 80 
-	if(temp >= 80) {
-		style ="hot";
-	}
-	// if the temp is less than 80 AND equal or greater than 65
-	else if(temp < 80 && temp >= 65) {
-		style ="warm";
-	}
-	// if the temp is less than 65 AND equal or greater than 40
-	else if(temp < 65 && temp >= 40) {
-		style ="chilly";
-	}
-  // if the temp is less than 40
-	else if(temp < 40) {
-		style = "cold"
-	}
-
-	// now 'style' should be one of four words that you can use in CSS
-	// this applies to styles to each div - the 'weather' one that all
-	// share, plus the 'style' one that customizes the background color
 	const output = `
-	<div class="weather ${style}">
-		${temp}
+	<div>
+		Hello There
 	</div>
 	`;
+
 	return(output)
 
 }
